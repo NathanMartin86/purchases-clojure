4f5252d1e475110376406380b4c0e5cecb41fd7a
macbookair <nj.martin123@gmail.com>
2015-11-24 14:24:55 -0500

    8.2

---
+++ core.clj
@@ -0,0 +1,31 @@
+(ns purchases-clojure.core
+  (:require [clojure.string :as str]
+            [clojure.walk :as walk])
+  (:gen-class))
+
+(defn -main [& args]
+
+  (let [purchases (slurp "purchases.csv")
+        purchases (str/split-lines purchases)
+        purchases (map (fn [line]
+                         (str/split line #","))
+                       purchases)
+        header (first purchases)
+        purchases (rest purchases)
+        purchases (map (fn [line]
+                         (interleave header line))
+                       purchases)
+        purchases (map (fn [line]
+                         (apply hash-map line))
+                       purchases)
+        purchases (walk/keywordize-keys purchases)]
+    (println "Please enter a category")
+    (let [input (read-line)
+          purchases (filter (fn [line]
+                              (= input (:category line)))
+                            purchases)]
+      (println purchases))
+    (spit "filtered_purchases.edn"
+          (pr-str purchases))))
+
+
